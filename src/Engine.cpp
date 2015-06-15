a755e98087a2fde156d6e92ea2bbfb0f960d2da8
Stephan Vedder <stephan.vedder@gmail.com>
2015-06-16 00:27:38 +0200

    Added initial splashscreen support

--- src/Engine.cpp
+++ src/Engine.cpp
@@ -5,12 +5,18 @@
 
 Engine::Engine()
 {
+	m_window.create(sf::VideoMode(800, 600), "openSAGE",sf::Style::None);
+	sf::Image splash;
+	bool loaded = splash.loadFromFile("GermanSplash.jpg");
+	sf::Texture splashTex;
+	splashTex.loadFromImage(splash);
+	m_window.draw(sf::Sprite(splashTex));
+	m_window.display();
 	Audio::Initialize();
 	FileSystem::Initialize();
 	stream.open("data\\audio\\speech\\mapunfolding.mp3");
 	mp3.openFromStream(stream);
 	mp3.play();
-	m_window.create(sf::VideoMode(800, 600), "openSAGE");
 }
 
 Engine::~Engine()
@@ -28,5 +34,7 @@ void Engine::Run()
             if (event.type == sf::Event::Closed)
                 m_window.close();
         }
+
+		m_window.display();
 	}
 }
