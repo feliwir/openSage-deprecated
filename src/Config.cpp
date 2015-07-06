ad611af26c35bf8bf3460d6b3c91e313365a8860
Stephan Vedder <stephan.vedder@gmail.com>
2015-07-06 21:28:27 +0200

    Added config file

---
+++ src/Config.cpp
@@ -0,0 +1,7 @@
+// Stephan Vedder 2015
+#include "Config.hpp"
+
+const uint32_t Config::Width = 1024;
+const uint32_t Config::Height = 768;
+const bool Config::Fullscreen = false;
+const std::string Config::Title = "sage";
---
+++ src/Config.hpp
@@ -0,0 +1,13 @@
+// Stephan Vedder 2015
+#pragma once
+#include <stdint.h>
+#include <string>
+
+class Config
+{
+public:
+	static const uint32_t Width;
+	static const uint32_t Height;
+	static const bool Fullscreen;
+	static const std::string Title;
+};
